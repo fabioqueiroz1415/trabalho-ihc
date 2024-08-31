1d62965d0b8cccba3523427a7d98f3d75de17a24
fabioqueiroz1415 <fabioaureliodedeus@gmail.com>
2024-08-31 19:27:06 -0300

    primeiro update

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.main a');
+    const screenContent = document.getElementById('screen-content');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Impede o comportamento padrão de abrir um link
+            const href = link.getAttribute('href');
+
+            loadContent(href);
+        });
+    });
+
+    function loadContent(href) {
+        // Faz uma requisição para o arquivo HTML
+        fetch(href)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(html => {
+                // Insere o conteúdo HTML na div.screen
+                screenContent.innerHTML = html;
+            })
+            .catch(error => {
+                // Trata erros, como arquivos não encontrados
+                screenContent.innerHTML = '<h1>Erro</h1><p>Não foi possível carregar o conteúdo.</p>';
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    }
+
+    loadContent('promocoes.html');
+});
