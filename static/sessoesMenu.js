30cb1b851fc306dc8aa42bae873e6f6b4b0cd432
fabioqueiroz1415 <fabioaureliodedeus@gmail.com>
2024-09-17 11:12:05 -0300

    alteração content onde é atualizado as sessões

--- static/sessoesMenu.js
+++ static/sessoesMenu.js
@@ -1,18 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  buttons = document.querySelectorAll('button');
-  buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-      id_mesa = document.body.id;
-      const item = button.parentNode.parentNode;
-      var id_item = item.id;
-      var nome_item = item.querySelector('.description').textContent;
-      var valor = button.value;
-      var quantidade = 1;
-      var path_imagem = item.querySelector('img').src;
-      add_pedido_carrinho(id_mesa, nome_item, id_item, valor, quantidade, path_imagem);
-    });
+buttons = document.querySelectorAll('button');
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    id_mesa = document.body.id;
+    const item = button.parentNode.parentNode;
+    var id_item = item.id;
+    var nome_item = item.querySelector('.description').textContent;
+    var valor = button.value;
+    var quantidade = 1;
+    var path_imagem = item.querySelector('img').src;
+    add_pedido_carrinho(id_mesa, nome_item, id_item, valor, quantidade, path_imagem);
   });
-})
+});
 
 function add_pedido_carrinho(id_mesa, nome_item, id_item, valor, quantidade, path_imagem) {
   var data = {
@@ -37,7 +35,7 @@ function add_pedido_carrinho_servidor(data) {
   .then(response => response.json())
   .then(d => {
     if (d.status === 200) {
-      console.log('Pedido adicionado com sucesso');
+      console.log('Pedido adicionado.');
     }
   })
   .catch((error) => {
