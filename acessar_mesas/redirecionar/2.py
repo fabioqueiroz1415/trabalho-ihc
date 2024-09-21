fda66e7ca66ceaf33c4d4d9e1033c9b8289f9da3
fabioqueiroz1415 <fabioaureliodedeus@gmail.com>
2024-09-21 09:49:41 -0300

    edit /redirecionar

--- acessar_mesas/redirecionar/2.py
+++ acessar_mesas/redirecionar/2.py
@@ -8,3 +8,5 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'
 import config as config
 
 webbrowser.open(f'{get_server_rote()}/m/{config.TABLES_TO_IDS[2]}')
+
+print(f'{get_server_rote()}/m/{config.TABLES_TO_IDS[2]}')
